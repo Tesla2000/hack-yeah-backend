f0e1cfd34a1d4f4c6e42d8fc9db1cf7f5a98a983
Tesla2000 <fratajczak124@gmail.com>
2025-10-04 17:56:22 +0200

    Added game turn, current phase and gender

--- runthroughlinehackathor/models/state.py
+++ runthroughlinehackathor/models/state.py
@@ -2,6 +2,9 @@ from typing import Union
 from uuid import UUID
 
 from pydantic import BaseModel
+from pydantic import NonNegativeInt
+from runthroughlinehackathor.models._gender import Gender
+from runthroughlinehackathor.models._phase import Phase
 from runthroughlinehackathor.models.action.action import Action
 from runthroughlinehackathor.models.action.reaction import Reaction
 from runthroughlinehackathor.models.parameters import Parameters
@@ -13,3 +16,6 @@ class State(BaseModel):
     parameters: Parameters
     history: list[Union[Action, RandomEvent, Reaction]]
     turn_descriptions: list[str]
+    current_phase: Phase
+    game_turn: NonNegativeInt
+    gender: Gender
