55c2c6777b5e71cb9e06c0cbcf95bcb167344e99
Tesla2000 <fratajczak124@gmail.com>
2025-10-05 07:48:40 +0200

    Added logger to main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
+import logging
 import os
+import traceback
 import uuid
 
 import uvicorn
@@ -23,8 +25,10 @@ from runthroughlinehackathor.models.state import State
 from runthroughlinehackathor.settings import settings
 from runthroughlinehackathor.state_update.state_increment import StateIncrement
 from runthroughlinehackathor.state_update.update_state import update_state
+from starlette.responses import PlainTextResponse
 from starlette.responses import RedirectResponse
 
+_logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
@@ -55,62 +59,80 @@ def api_key_auth(x_api_key: str = Depends(X_API_KEY)):
 
 @app.post("/create-new-game", dependencies=[Depends(api_key_auth)])
 async def create_new_game(create_new_game_input: _CreateNewGameInput):
-    gender_text = {Gender.MALE: "mężczyzną", Gender.FEMALE: "kobietą"}[
-        create_new_game_input.gender
-    ]
-    age_text = f"{settings.initial_age} letni" + (
-        "m" if create_new_game_input.gender == Gender.MALE else "ą"
-    )
-    history = []
-    current_stage = Stage.FIRST
-    parameters = Parameters(
-        health=settings.initial_health,
-        career=settings.initial_other_parameters,
-        relations=settings.initial_other_parameters,
-        money=settings.initial_other_parameters,
-    )
-    actions, random_event = await asyncio.gather(
-        select_actions(
-            history=history, current_stage=current_stage, parameters=parameters
-        ),
-        select_random_event(history),
-    )
-    new_state = State(
-        id=uuid.uuid4(),
-        parameters=parameters,
-        history=[random_event],
-        turn_description=settings.initial_turn_description.format(
-            gender=gender_text, age=age_text
-        ),
-        current_stage=current_stage,
-        game_turn=0,
-        gender=create_new_game_input.gender,
-        goal=create_new_game_input.goal,
-        name=create_new_game_input.name,
-        big_actions=list(
-            a for a in actions if a.time_cost > settings.small_action_max_cost
-        ),
-        small_actions=list(
-            a for a in actions if a.time_cost <= settings.small_action_max_cost
-        ),
-        random_event=random_event,
-    )
-    states.append(new_state)
-    return JSONResponse(
-        content=new_state.model_dump(mode="json"), status_code=201
-    )
+    try:
+        gender_text = {Gender.MALE: "mężczyzną", Gender.FEMALE: "kobietą"}[
+            create_new_game_input.gender
+        ]
+        age_text = f"{settings.initial_age} letni" + (
+            "m" if create_new_game_input.gender == Gender.MALE else "ą"
+        )
+        history = []
+        current_stage = Stage.FIRST
+        parameters = Parameters(
+            health=settings.initial_health,
+            career=settings.initial_other_parameters,
+            relations=settings.initial_other_parameters,
+            money=settings.initial_other_parameters,
+        )
+        actions, random_event = await asyncio.gather(
+            select_actions(
+                history=history,
+                current_stage=current_stage,
+                parameters=parameters,
+            ),
+            select_random_event(history),
+        )
+        new_state = State(
+            id=uuid.uuid4(),
+            parameters=parameters,
+            history=[random_event],
+            turn_description=settings.initial_turn_description.format(
+                gender=gender_text, age=age_text
+            ),
+            current_stage=current_stage,
+            game_turn=0,
+            gender=create_new_game_input.gender,
+            goal=create_new_game_input.goal,
+            name=create_new_game_input.name,
+            big_actions=list(
+                a
+                for a in actions
+                if a.time_cost > settings.small_action_max_cost
+            ),
+            small_actions=list(
+                a
+                for a in actions
+                if a.time_cost <= settings.small_action_max_cost
+            ),
+            random_event=random_event,
+        )
+        states.append(new_state)
+        return JSONResponse(
+            content=new_state.model_dump(mode="json"), status_code=201
+        )
+    except Exception:
+        _logger.error(traceback.format_exc())
+        return PlainTextResponse(traceback.format_exc())
 
 
 @app.post("/next-turn", dependencies=[Depends(api_key_auth)])
 async def get_next_state(state_update: StateIncrement):
-    state = next((s for s in states if s.id == state_update.state_id), None)
-    if state is None:
-        return HTTPException(
-            detail=f"No state with id={state_update.state_id}",
-            status_code=404,
+    try:
+        state = next(
+            (s for s in states if s.id == state_update.state_id), None
         )
-    await update_state(state, state_update)
-    return JSONResponse(content=state.model_dump(mode="json"), status_code=200)
+        if state is None:
+            return HTTPException(
+                detail=f"No state with id={state_update.state_id}",
+                status_code=404,
+            )
+        await update_state(state, state_update)
+        return JSONResponse(
+            content=state.model_dump(mode="json"), status_code=200
+        )
+    except Exception:
+        _logger.error(traceback.format_exc())
+        return PlainTextResponse(traceback.format_exc())
 
 
 @app.get("/")
