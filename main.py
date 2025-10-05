5ec7ba32d9fedce32f5eb30ebdd7c531d6fa3c1e
Tesla2000 <fratajczak124@gmail.com>
2025-10-05 09:59:26 +0200

    Updated prompt

--- main.py
+++ main.py
@@ -60,9 +60,6 @@ def api_key_auth(x_api_key: str = Depends(X_API_KEY)):
 @app.post("/create-new-game", dependencies=[Depends(api_key_auth)])
 async def create_new_game(create_new_game_input: _CreateNewGameInput):
     try:
-        age_text = f"{settings.initial_age} letni" + (
-            "m" if create_new_game_input.gender == Gender.MALE else "ą"
-        )
         history = []
         current_stage = Stage.FIRST
         parameters = Parameters(
@@ -84,7 +81,7 @@ async def create_new_game(create_new_game_input: _CreateNewGameInput):
             parameters=parameters,
             history=[random_event],
             turn_description=settings.initial_turn_description.format(
-                age=age_text
+                age=settings.initial_age
             ),
             current_stage=current_stage,
             game_turn=0,
